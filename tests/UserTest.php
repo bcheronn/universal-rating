11114bb8f28d10b7c78796f9a7f731fde30d7da0
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-08-21 09:09:03 +0200

    Formatting update

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
+namespace UniversalRating\Tests;
+
 use UniversalRating\User;
+use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
