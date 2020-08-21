bfbae6631995ee1349312f4f08e9b3ebe8146625
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-08-21 13:59:12 +0200

    Initial hone page

--- public/index.php
+++ public/index.php
@@ -6,7 +6,33 @@ require '../vendor/autoload.php';
 
 use UniversalRating\User;
 
-echo "Universal Rating</br>";
-
 $user = new User;
-$user->echoEmail();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Universal Rating</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body class="text-light bg-dark">
+    <div class="container">
+        <nav class="navbar sticky-top navbar-expand navbar-dark">
+            <a class="navbar-brand" href="#">Universal Rating</a>
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#">Home</a>
+                <a class="nav-item nav-link" href="#">Sign In</a>
+                <a class="nav-item nav-link" href="#">Rate Anything</a>
+            </div>
+        </nav>
+        <main>
+            Logged user: <?= $user->echoEmail() ?>
+        </main>
+    </div>
+</body>
+
+</html>
