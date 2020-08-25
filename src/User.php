07ad6e646640887213fda044d770a5532bffc343
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-08-25 12:02:53 +0200

    User name test failing

--- src/User.php
+++ src/User.php
@@ -7,9 +7,15 @@ namespace UniversalRating;
 class User
 {
     private $email = 'john@doe.com';
-    private $userName = "John Doe";
+    private $userName;
 
-    public function echoEmail()
+    public function __construct($userName = "John Doe")
+    {
+        $this->userName = $userName;
+    }
+
+    // Echo user mail (used for autoload testing)
+    public function echoEmail(): void
     {
         echo $this->email, PHP_EOL;
     }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,16 +9,26 @@ use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
+    public function testEchoUserEmail(): void
+    {
+        $user = new User;
+        $this->expectOutputString("john@doe.com" . PHP_EOL);
+        $user->echoEmail();
+    }
+
     public function testClassUserExists(): void
     {
         $this->assertInstanceOf(
             User::class,
-            new User
+            new User,
+            "User class cannot be instantiated"
         );
     }
 
     public function testUserHasAName(): void
     {
-        $this->assertClassHasAttribute("userName", User::class, "Class user has no user name");
+        $this->assertClassHasAttribute("userName", User::class, "User class has no user name");
+        $user = new User("James SMITH");
+        $this->assertIsString($user->getUserName(), "User name is not a string");
     }
 }
